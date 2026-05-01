10b67a291e067696ed2790257540111aed2e53fa
anji-mothukuri <anjimothukuri674@gmail.com>
2026-05-01 18:53:43 +0530

    selectors and locators

---
+++ tests/locators.spec.ts
@@ -0,0 +1,16 @@
+import {test, expect} from '@playwright/test'
+
+test("Selectors and Locators", async ({page}) => {
+    await page.goto("https://www.saucedemo.com/");
+    await page.locator('[data-test="username"]').fill("locator by playwright");
+
+    await page.locator("//input[@id='password']").fill("locator by xpath");
+    await page.locator("xpath=//input[@id='password']").fill("locator by xpath - 2");
+
+    await page.locator("id=user-name").fill("locator known");
+
+    await page.locator('text=login').click();
+    await page.locator("input:has-text('login')").click();
+
+    await page.pause();
+})
